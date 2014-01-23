3b94558c10638698453a95877681b1633938b6ff
g_rocket <g.rocket1@gmail.com>
2014-01-23 10:25:33 -0500

    refactor code, make work with 12-bit data and 6-bit id

--- src/net/clonecomputers/greenroof/GreenroofServer.java
+++ src/net/clonecomputers/greenroof/GreenroofServer.java
@@ -3,10 +3,7 @@ package net.clonecomputers.greenroof;
 import java.io.*;
 import java.net.*;
 import java.sql.*;
-import java.text.*;
 import java.util.*;
-import java.util.Map.Entry;
-import java.util.Date;
 import java.util.concurrent.*;
 
 public class GreenroofServer {
@@ -19,19 +16,11 @@ public class GreenroofServer {
 		    throw new RuntimeException("Cannot find the driver in the classpath!", e);
 		}
 	}
-
-	private double etape_m = -1/40D, etape_b = 16;
-	private double ord_b = .0908, ord_e = 5.1327;
-	private double other_resistor = 1000; // ohms
-	private double v_in = 3.3; // volts
 	
 	private String insertString = 
 			"INSERT INTO `greenroof-monitor`"+
 			"(`timestamp`, `raw timestamp`, `raw value`, `processed value`)"+
 			" VALUES ('%s',%d,%d,%.2f);";
-
-	private DateFormat dateFormat =
-			new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
 	private ServerSocket server;
 	private ExecutorService exec;
@@ -85,31 +74,19 @@ public class GreenroofServer {
 		
 		public void run(){
 			System.out.println("Processing Data");
-			List<SensorReading> readings = readData(socket);
+			List<SensorReading> readings = readData(socket, rTime);
 			System.out.println("raw: " + readings);
 			try {
 				socket.close();
 			} catch (IOException e) {
 				throw new RuntimeException(e);
 			}
-			short[] rawData = new short[readings.size()];
-			int[] times = new int[readings.size()];
-			
-			for(int i = 0; readings.size() >= 0; i++){
-				Entry<Integer,Short> e = readings.pollFirstEntry();
-				// potential problems, as it removes the entry
-				// and entries aren't guarenteed to work unless
-				// they are still mapped in the map
-				if(e == null) break;
-				times[i] = e.getKey();
-				rawData[i] = e.getValue();
-			}
-			System.out.println("semi-processed:" + Arrays.toString(rawData) +","+ Arrays.toString(times));
-			processAndInsertData(rawData, times, rTime);
+			//System.out.println("semi-processed:" + readings);
+			insert(readings);
 		}
 	}
 	
-	public List<SensorReading> readData(Socket socket){
+	public List<SensorReading> readData(Socket socket, long connectionStartTime){
 		List<SensorReading> readings = new LinkedList<SensorReading>();
 		BufferedInputStream input;
 		try {
@@ -142,22 +119,24 @@ public class GreenroofServer {
 					throw new IOException("bad time (too big): " +
 							Arrays.toString(data));
 				}
-				int time = 
+				long time = 
 						((data[0] & 0x3f) << 25) |
 						((data[1] & 0x7f) << 18) |
 						((data[2] & 0x7f) << 11) |
 						((data[3] & 0x7f) << 04) |
 						((data[4] & 0x78) >> 3);
-				short reading = (short)
-						(((data[4] & 0x07) << 7) |
-						(data[5] & 0x7f));
-				System.out.println("read one line: " + "("+time+", "+reading+")");
-				readings.add(new SensorReading(data, time, id));
-				System.out.println("put in map");
-				System.out.printf("data[6] = 0x%x\n",data[6]);
-				System.out.printf("data[6] == 0xff -> %b\n", data[6] == (byte)0xff);
+				short reading = (short)(
+						((data[4] & 0x07) << 8) |
+						((data[5] & 0x7f) << 1) |
+						((data[6] & 0x80) >> 6));
+				byte id = (byte)(data[6] & 0x3f);
+				//System.out.println("read one line: " + "("+time+", "+reading+","+id+")");
+				readings.add(new SensorReading(reading, connectionStartTime - time, id));
+				//System.out.println("put in map");
+				//System.out.printf("data[6] = 0x%x\n",data[6]);
+				//System.out.printf("data[6] == 0xff -> %b\n", data[6] == (byte)0xff);
 				if(data[6] == (byte)0xff) stopByteWritten = true;
-				System.out.println("socket.isClosed() = " + socket.isClosed() + ", stopByteWritten = " + stopByteWritten);
+				//System.out.println("socket.isClosed() = " + socket.isClosed() + ", stopByteWritten = " + stopByteWritten);
 			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
@@ -177,35 +156,6 @@ public class GreenroofServer {
 		return sb.toString();
 	}
 	
-	public void processAndInsertData(short[] rawData, int[] times, long sTime){
-		double[] processedData = new double[rawData.length];
-		long[] absoluteTimes = new long[times.length];
-		String[] timeStrings = new String[times.length];
-		for(int i = 0; i < rawData.length; i++){
-			processedData[i] = process(rawData[i]);
-		}
-		for(int i = 0; i < times.length; i++){
-			absoluteTimes[i] = sTime - times[i];
-			timeStrings[i] = getTimeString(times[i], sTime);
-		}
-		insert(timeStrings, absoluteTimes, rawData, processedData);
-	}
-	
-	public String getTimeString(int time, long sTime){
-		// time is milliseconds before sent, sTime is time sent (in ms since 1970)
-		long aTime = sTime - time; // absolute time
-		return dateFormat.format(new Date(aTime));
-	}
-	
-	public double process(short raw){
-		double v_out = raw*(v_in/1024D); // volts
-		double resistance = (other_resistor*((v_in/v_out) - 1)); // ohms
-		double levelI = etape_m*resistance + etape_b; // inches
-		double levelF = levelI/12D; // feet
-		double rate = ord_b*Math.exp(levelF*ord_e); //   gallons / minuite
-		return rate;
-	}
-	
 	public void insert(long time, short raw, double processed) {
 		insert("CURRENT_TIMESTAMP",time,raw,processed);
 	}
@@ -215,12 +165,15 @@ public class GreenroofServer {
 		executeSql(String.format(insertString,timestamp,time,raw,processed));
 	}
 	
-	public void insert(String[] timestamps,
-			long[] times, short[] raws, double[] processeds){
-		String[] sqlStrings = new String[raws.length];
-		for(int i = 0; i < sqlStrings.length; i++){
-			sqlStrings[i] = String.format(insertString,
-					timestamps[i],times[i],raws[i],processeds[i]);
+	public String getSqlString(SensorReading r) {
+		return String.format(insertString, r.timeString, r.timestamp, r.data, r.processedData);
+	}
+	
+	public void insert(List<SensorReading> readings){
+		String[] sqlStrings = new String[readings.size()];
+		int i = 0;
+		for(SensorReading r: readings){
+			sqlStrings[i++] = getSqlString(r);
 		}
 		executeSqls(sqlStrings);
 	}
--- src/net/clonecomputers/greenroof/SensorReading.java
+++ src/net/clonecomputers/greenroof/SensorReading.java
@@ -1,19 +1,47 @@
 package net.clonecomputers.greenroof;
 
+import java.text.*;
+import java.util.*;
+
 public class SensorReading {
 	public final short data;
 	public final long timestamp;
-	public final byte sensorID; // low 7 bits
+	public final byte sensorID; // low 6 bits
+	public final double processedData;
+	public final String timeString;
+	
+	private static DateFormat dateFormat =
+			new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
 	/**
 	 * 
-	 * @param data 12-bit
-	 * @param timestamp unsigned int
-	 * @param sensorID low 7 bits only
+	 * @param data 12-bit sensor data
+	 * @param timestamp 32-bit unsigned int
+	 * @param sensorID low 6 bits only
 	 */
 	public SensorReading(short data, long timestamp, byte sensorID) {
 		this.data = data;
 		this.timestamp = timestamp;
 		this.sensorID = sensorID;
+		this.processedData = processData(data,sensorID);
+		timeString = dateFormat.format(new Date(timestamp));
+	}
+	
+	public String toString() {
+		return String.format("(%d,%d,%d,%.4f)",data,timestamp,sensorID,processedData);
+	}
+	
+	private static final double etape_m = -1/40D, etape_b = 16;
+	private static final double ord_b = .0908, ord_e = 5.1327;
+	private static final double other_resistor = 1000; // ohms
+	private static final double v_in = 3.3; // volts
+	
+	public static double processData(short rawData, byte sensorID){
+		double v_out = rawData*(v_in/1024D); // volts
+		double resistance = (other_resistor*((v_in/v_out) - 1)); // ohms
+		double levelI = etape_m*resistance + etape_b; // inches
+		double levelF = levelI/12D; // feet
+		double rate = ord_b*Math.exp(levelF*ord_e); //   gallons / minuite
+		return rate;
 	}
 }
